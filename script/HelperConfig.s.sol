3beabb5dce31ed1a74bdd07f455589dd204c0588
Valentina De Paolis <valedepaolis@gmail.com>
2025-04-30 21:09:55 +0200

    add mock

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Raffle} from "../src/Raffle.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 abstract contract CodeConstants {
+    /* VRF mock valuesNB; changed from video, just 2 params*/
+    uint96 public MOCK_BASE_FEE = 0.25 ether; // 0.25 LINK per request
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9; // 1 LINK per gas
+
+
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1155511;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -43,8 +48,22 @@ contract HelperConfig is CodeConstants, Script{
         if (localNetworkConfig.vrfCoordinator != address(0)){
             return localNetworkConfig;
         } 
-
         // deplpy mocks and such
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorMock = new VRFCoordinatorV2Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK);
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, // 30 seconds
+            vrfCoordinator: address(vrfCoordinatorMock),
+            // does not matter
+            gasLane: vrfCoordinatorMock.createKeyHash(),
+            callbackGasLimit: 500000,
+            subscriptionId: 0 // might be fixed
+        });
+
+        return localNetworkConfig;
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkCofig){
