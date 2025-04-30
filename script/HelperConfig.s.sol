a07ee26a788ea05891cb01aaecab7f2e070312aa
Valentina De Paolis <valedepaolis@gmail.com>
2025-04-30 20:53:31 +0200

    add deploy script and helper config (mocks to add)

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+
+abstract contract CodeConstants {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1155511;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
+
+contract HelperConfig is CodeConstants, Script{
+
+    error HelperConfig__InvalidChainId();
+
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint32 callbackGasLimit;
+        uint256 subscriptionId;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfig;  
+
+    constructor(){
+        networkConfig[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory){
+        if (networkConfigs[chainId].vrfCoordinator != address(0)){
+            return networkConfigs[chainId];
+        } else if (chainId == LOCAL_CHAIN_ID){
+            return getOrCreateAnvilEthConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    function getOrCreateAnvilEthConfig() public returs (NetworkConfig){
+        if (localNetworkConfig.vrfCoordinator != address(0)){
+            return localNetworkConfig;
+        } 
+
+        // deplpy mocks and such
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkCofig){
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, // 30 seconds
+            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000,
+            subscriptionId: 0});
+    }
+}
